f0889a0a9ff359683c89b4f9855621e47b9ad4b9
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2022-12-01 15:36:55 +0100

    DoesNotDoAnything

---
+++ src/main/java/com/example/testing/DoesNotDoAnything.java
@@ -0,0 +1,10 @@
+package com.example.testing;
+
+/* carlpeters created on 01/12/2022 inside the package - com.example.testing */
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DoesNotDoAnything {
+//    log.debug("");
+}
