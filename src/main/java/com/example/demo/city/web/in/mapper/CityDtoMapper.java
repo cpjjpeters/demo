896d7ab6d5e9ee3b72c9d2251af96ca4303c05ca
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2022-12-12 16:54:06 +0100

    added demo-api module

--- src/main/java/com/example/demo/city/web/in/mapper/CityDtoMapper.java
+++ src/main/java/com/example/demo/city/web/in/mapper/CityDtoMapper.java
@@ -2,9 +2,10 @@ package com.example.demo.city.web.in.mapper;
 
 /* carlpeters created on 10/12/2022 inside the package - com.example.demo.city.web.in.mapper */
 
+import com.example.demo.common.web.in.mapper.BaseMapStructConfig;
 import org.mapstruct.Mapper;
 
-//@Mapper(componentModel = "spring", config = BaseMapStructConfig.class)
+@Mapper(componentModel = "spring", config = BaseMapStructConfig.class)
 public class CityDtoMapper {
 
 //    CityResource mapDomainToResource(City city);
